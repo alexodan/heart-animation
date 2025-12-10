d6f27d2534752670eab394e8105b5d8cf4ca81a5
Alexis Diaz <alexdan1993@gmail.com>
2025-12-10 09:13:31 -0300

    Simplifying particle animation

--- src/components/heart/Heart.tsx
+++ src/components/heart/Heart.tsx
@@ -16,22 +16,23 @@ export function Heart() {
         className={`${styles.circle} ${isActive ? styles.animating : ''}`}
       />
       <HeartIcon isActive={isActive} />
-      {Array.from({ length: 12 }).map((_, i) => {
-        const angle = (i * 360) / 12
-        const distance = i % 2 ? 40 : 50
-        return (
-          <span
-            key={i}
-            style={
-              {
-                '--angle': `${angle}deg`,
-                '--distance': `-${distance}px`,
-              } as React.CSSProperties
-            }
-            className={`${styles.particle} ${isActive ? styles.animating : ''}`}
-          />
-        )
-      })}
+      {isActive &&
+        Array.from({ length: 12 }).map((_, i) => {
+          const angle = (i * 360) / 12
+          const distance = i % 2 ? 40 : 50
+          return (
+            <span
+              key={i}
+              style={
+                {
+                  '--angle': `${angle}deg`,
+                  '--distance': `-${distance}px`,
+                } as React.CSSProperties
+              }
+              className={styles.particle}
+            />
+          )
+        })}
     </button>
   )
 }
